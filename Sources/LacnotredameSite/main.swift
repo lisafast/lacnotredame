db396ab1b2173c3affd38086d7a9c03523514fe5
Steve Hume <stevelist@hume.ca>
2021-04-19 16:12:23 -0400

    dev settings
    dev settings

--- Sources/LacnotredameSite/main.swift
+++ Sources/LacnotredameSite/main.swift
@@ -54,7 +54,7 @@ try LacnotredameSite().publish(
       formatter.timeZone = TimeZone(secondsFromGMT: 0)
       context.dateFormatter = formatter
     },
-    .if(true, .installPlugin(.markdownLint)),
+    .if(false, .installPlugin(.markdownLint)),
     .addMarkdownFiles(),
 //    .add404Page(),
     .sortItems(by: \.date, order: .descending),
@@ -68,7 +68,7 @@ try LacnotredameSite().publish(
     //        },
     .moveWebsitePages(paths: [("404","404.html")]),
     .generateSiteMap(excluding: ["main","404"], indentedBy: nil),
-    .if(true, .installPlugin(.compressFiles)),
+    .if(false, .installPlugin(.compressFiles)),
     
     //        .unwrap(nil, PublishingStep.deploy)
   ],
